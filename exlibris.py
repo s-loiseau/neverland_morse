32aaec47161e273f3938f1328e334eef4cc3d5f3
s-loiseau <tjahl013@gmail.com>
2021-03-27 14:24:43 +0100

    class ok

--- exlibris.py
+++ exlibris.py
@@ -1,141 +1,155 @@
 #!/usr/bin/env python
 
-from PIL import Image, ImageDraw, ImageFilter
+from PIL import Image, ImageDraw
 import random
 import subprocess
 import sys
 
 
-morsedict = {
-    "A": "01",
-    "B": "1000",
-    "C": "1010",
-    "D": "100",
-    "E": "0",
-    "F": "0010",
-    "G": "110",
-    "H": "0000",
-    "I": "00",
-    "J": "0111",
-    "K": "101",
-    "L": "0100",
-    "M": "11",
-    "N": "10",
-    "O": "111",
-    "P": "0110",
-    "Q": "1101",
-    "R": "010",
-    "S": "000",
-    "T": "1",
-    "U": "001",
-    "V": "0001",
-    "W": "011",
-    "X": "1001",
-    "Y": "1011",
-    "Z": "1100",
-    "1": "01111",
-    "2": "00111",
-    "3": "00011",
-    "4": "00001",
-    "5": "00000",
-    "6": "10000",
-    "7": "11000",
-    "8": "11100",
-    "9": "11110",
-    "0": "11111",
-    ",": "110011",
-    ".": "010101",
-    "?": "001100",
-    "/": "10010",
-    "-": "100001",
-    "(": "10110",
-    ")": "101101",
-    " ": "_",
-}
+class exlibris:
+    def __init__(self, image_path, message):
+        self.message = message
+        self.image_path = image_path
+
+        self.morsedict = {
+            "A": "01",
+            "B": "1000",
+            "C": "1010",
+            "D": "100",
+            "E": "0",
+            "F": "0010",
+            "G": "110",
+            "H": "0000",
+            "I": "00",
+            "J": "0111",
+            "K": "101",
+            "L": "0100",
+            "M": "11",
+            "N": "10",
+            "O": "111",
+            "P": "0110",
+            "Q": "1101",
+            "R": "010",
+            "S": "000",
+            "T": "1",
+            "U": "001",
+            "V": "0001",
+            "W": "011",
+            "X": "1001",
+            "Y": "1011",
+            "Z": "1100",
+            "1": "01111",
+            "2": "00111",
+            "3": "00011",
+            "4": "00001",
+            "5": "00000",
+            "6": "10000",
+            "7": "11000",
+            "8": "11100",
+            "9": "11110",
+            "0": "11111",
+            ",": "110011",
+            ".": "010101",
+            "?": "001100",
+            "/": "10010",
+            "-": "100001",
+            "(": "10110",
+            ")": "101101",
+            " ": "_",
+        }
+
+    def save(self):
+        # savefile
+        self.savefile = f"results/{self.message}.png"
+        self.image.save(self.savefile)
+
+    def show(self):
+        # subprocess.run(["feh", "-B", "white", self.filename])
+        subprocess.run(["feh", self.savefile])
+
+    def get_image_info(self):
+        """return palette"""
+        self.width, self.height = self.image.size
+
+        quantized = self.image.quantize(colors=2, kmeans=3)
+        convert_rgb = quantized.convert("RGB")
+        self.colors = convert_rgb.getcolors(self.width * self.height)
+        self.r, self.g, self.b = self.colors[0][1]
+
+    def draw(self):
+        # encode message
+        self.morse()
+
+        self.image = Image.open(self.image_path)
+        self.cursor = -180
+
+        self.long = 1
+        self.short = 0.2
+        self.halfspace = 0.08
+
+        self.output = ImageDraw.Draw(self.image)
+
+        self.set_ratio()
+        self.get_image_info()
+
+        self.arcwidth = 8
+
+        ink = (self.r, self.g, self.b, 255)
+        paper = (self.r, self.g, self.b, 150)
+
+        def drawarc(arclength, color):
+            # draw arc and update cursor
+            self.output.arc(
+                (0, 0, self.width, self.height),
+                start=self.cursor,
+                end=self.cursor + arclength,
+                fill=color,
+                width=self.arcwidth,
+            )
+            self.cursor = self.cursor + arclength
+
+        # draw cirle
+        drawarc(360, paper)
+        # loop draw arc.
+        for x in self.messagemorse:
+            if x == "0":
+                drawarc(self.ratio * self.short, ink)
+            elif x == "1":
+                drawarc(self.ratio * self.long, ink)
+            elif x in [" ", "_"]:
+                self.cursor += self.ratio * self.halfspace
+
+            self.cursor += self.ratio * self.halfspace
+
+    def morse(self):
+        # define messagemorse
+        self.messagemorse = ""
+        for c in self.message:
+            self.messagemorse += self.morsedict[c] + " "
+
+    def set_ratio(self):
+        # define ratio
+        longueur = 0
+        for x in self.messagemorse:
+            if x == "0":
+                longueur += self.short + self.halfspace
+            elif x == "1":
+                longueur += self.long + self.halfspace
+            elif x in [" ", "_"]:
+                longueur += 2 * self.halfspace
+        self.ratio = 360 / longueur
 
 
-def getpalette(image_path):
-    """return palette and size."""
-    image = Image.open(image_path)
-    width, height = image.size
-    quantized = image.quantize(colors=2, kmeans=3)
-    convert_rgb = quantized.convert("RGB")
-    colors = convert_rgb.getcolors(width * height)
-    print(width, height)
-    return (width, height, colors)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        message = sys.argv[1]
+    else:
+        message = "FIREFLY"
 
+    image_path = "ressources/owlraw.png"
 
-def drawarc(length, width, color):
-    global cursor
-    draw.arc(dimension, start=cursor, end=cursor + length, fill=color, width=width)
-    cursor = cursor + length
-
-
-def morse(message):
-    messagemorse = ""
-    for c in messageclear:
-        print(c, morsedict[c])
-        messagemorse += morsedict[c] + " "
-    return messagemorse
-
-
-def longueur_message(messagemorse):
-    global short
-    global long
-    global halfspace
-
-    longueur = 0
-    for x in messagemorse:
-        if x == "0":
-            longueur += short + halfspace
-        elif x == "1":
-            longueur += long + halfspace
-        elif x in [" ", "_"]:
-            longueur += 2 * halfspace
-    return longueur
-
-
-short = 0.2
-long = 1
-halfspace = 0.08
-pen_width = 8
-cursor = -180
-
-if len(sys.argv) > 1:
-    messageclear = sys.argv[1]
-else:
-    messageclear = "FIREFLY"
-
-messagemorse = morse(messageclear)
-longueur = longueur_message(messagemorse)
-
-ratio = 360 / longueur
-
-filepath = "ressources/owlraw.png"
-owl = Image.open(filepath)
-width, height, palette = getpalette(filepath)
-
-draw = ImageDraw.Draw(owl)
-
-r, g, b = palette[0][1]
-
-ink = (r + 18, g + 18, b + 18, 250)
-paper = (r + 9, g + 9, b + 9, 200)
-dimension = (0, 0, width, height)
-
-# draw clear circle
-drawarc(360, pen_width, paper)
-# drawarc(360, 10, paper)
-
-for x in messagemorse:
-    if x == "0":
-        drawarc(ratio * short, pen_width, ink)
-    elif x == "1":
-        drawarc(ratio * long, pen_width, ink)
-    elif x == " ":
-        cursor += ratio * halfspace
-    cursor += ratio * halfspace
-
-filename = f"results/{messageclear}.png"
-owl.save(filename)
-subprocess.run(["feh", "-B", "black", filename])
+    result = exlibris(image_path, message)
+    # result.morse()
+    result.draw()
+    result.save()
+    result.show()
